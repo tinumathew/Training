9e1cd7d9322123987a2c8a46b9b6f05c312fa89e
Tinu Mathew <tinu@qburst.com>
2015-09-15 14:46:14 +0530

    Revised jQuery code on 15/09/2015

--- ShoppingCart/script/script.js
+++ ShoppingCart/script/script.js
@@ -1,5 +1,5 @@
 var i = 0, totalAmount = 0, currentItem = 0;
-var images = ["assets/dress.jpg", "assets/footwear.jpeg", "assets/handbags.JPG", "assets/laptop.jpg", "assets/ornaments.jpg", "assets/teddy.jpg", "assets/orange.jpg", "assets/chocolate.jpg", "assets/wheat.jpg"];
+var images = ["../assets/dress.jpg", "../assets/footwear.jpeg", "../assets/handbags.JPG", "../assets/laptop.jpg", "../assets/ornaments.jpg", "../assets/teddy.jpg", "../assets/orange.jpg", "../assets/chocolate.jpg", "../assets/wheat.jpg"];
 var item = ["Frock", "Men's Shoes", "Leather Handbag", "Lenovo Laptop", "Red Ruby Jewellery", "Teddy Bear", "Oranges", "Diary Milk", "Cream Wheat Cinnabon"];
 var price = [850, 1200, 450, 25500, 150000, 500, 50, 10, 275];
 var count = [5, 12, 3, 10, 2, 8, 20, 30, 35];
@@ -7,11 +7,11 @@ var cartItem = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 var itemObj = [];
 function displayItems(i) {
     "use strict";
-    document.getElementById("caption").innerHTML = "Item Details";
-    document.getElementById("iname").innerHTML = "Item Name:" + itemObj[i].item;
-    document.getElementById("iprice").innerHTML = "Item Price:" + itemObj[i].price;
-    document.getElementById("icount").innerHTML = "Item Count:" + itemObj[i].count;
-    document.getElementById("itotal").innerHTML = "Total Amount:" + totalAmount;
+    $("#caption").html("Item Details");
+    $("#iname").html("Item Name:" + itemObj[i].item);
+    $("#iprice").html("Item Price:" + itemObj[i].price);
+    $("#icount").html("Item Count:" + itemObj[i].count);
+    $("#itotal").html("Total Amount:" + totalAmount);
     currentItem = i;
 }
 function Product(item, price) {
@@ -45,47 +45,28 @@ function ProductChild(name, price, count, images) {
         }
     }
 }
-ProductChild.prototype = Object.create(Product.prototype);
-ProductChild.prototype.constructor = ProductChild;
+ProductChild.prototype = new Product();
 for (i = 0; i < 9; i += 1) {
     itemObj[i] = new ProductChild(item[i], price[i], count[i], images[i]);
 }
 function start() {
     "use strict";
     for (i = 0; i < 9; i += 1) {
-        var division = document.createElement("div");
-        var t = document.createElement("img");
-        t.setAttribute("src", images[i]);
-        t.setAttribute("height", "200");
-        t.setAttribute("width", "200");
-        t.setAttribute("alt", "Image");
-        t.setAttribute("id", i);
-        t.setAttribute("style", "float:left;padding:10px;");
-        division.appendChild(t);
-        document.getElementById("tile").appendChild(division);
-        document.getElementById(i).addEventListener("click", function (it) {
-            return function () {
-                displayItems(it);
-            }
-        }
-            (i));
+        var division = $("<div></div>"), t = $("<img>");
+        division.attr("id", i);
+        $("#tile").append(division);
+        $(t).attr({src: images[i], alt: "Image", id: i}).addClass("imag");
+        $("#" + i).append(t);
     }
 }
 $(document).ready(function () {
     "use strict";
-    $("#tile").mouseover(function () {
-        $("img").mouseover(function (t) {
-            var ele = t.currentTarget.id;
-            $("#" + ele).css("opacity", "0.2");
-        });
-        $("img").mouseout(function (e) {
-            var ele = e.currentTarget.id;
-            $("#" + ele).css("opacity", "1");
+    $("#tile").mouseenter(function () {
+        $(".imag").bind("click", function (e) {
+            displayItems(e.currentTarget.id);
         });
     });
-});
-$(document).ready(function () {
-    "use strict";
+    
     $("#add , #cancel").focusin(function () {
         $(this).css("background-color", "white");
         $(this).css("color", "blue");
@@ -95,5 +76,5 @@ $(document).ready(function () {
         $(this).css("color", "white");
     });
 });
-document.getElementById("add").addEventListener("click", itemObj[currentItem].addToCart);
-document.getElementById("cancel").addEventListener("click", itemObj[currentItem].cancelFromCart);
+$("#add").click(itemObj[currentItem].addToCart);
+$("#cancel").click(itemObj[currentItem].cancelFromCart);
